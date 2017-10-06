00e2ec493c2eb5ada4ad0dd1ff8adc9e5cd4ab1a
Mischuk Alexander <mischuk.alexander@gmail.com>
2017-10-06 16:11:31 +0300

    images -fix, qr popup -add

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -2,6 +2,41 @@ $(function() {
     $('a[href="#"]').click(function(e){
       e.preventDefault();
     });
+    $.extend(true, $.magnificPopup.defaults, {
+      tClose: 'Закрыть (Esc)', // Alt text on close button
+      tLoading: 'Loading...', // Text that is displayed during loading. Can contain %curr% and %total% keys
+      gallery: {
+        tPrev: 'Previous (Left arrow key)', // Alt text on left arrow
+        tNext: 'Next (Right arrow key)', // Alt text on right arrow
+        tCounter: '%curr% of %total%' // Markup for "1 of 7" counter
+      },
+      image: {
+        tError: '<a href="%url%">The image</a> could not be loaded.' // Error message when image could not be loaded
+      },
+      ajax: {
+        tError: '<a href="%url%">The content</a> could not be loaded.' // Error message when ajax request failed
+      }
+    });
+    $('.popup-promo').magnificPopup({
+        type: 'inline',
+
+        fixedContentPos: false,
+        fixedBgPos: true,
+
+        overflowY: 'auto',
+
+        closeBtnInside: true,
+        preloader: false,
+
+        midClick: true,
+        removalDelay: 300,
+        mainClass: 'my-mfp-zoom-in'
+    });
+
+    $('#send-qr').on('submit', function(){
+      $('#popup-send-qr-call').trigger('click');
+      return false;
+    });
 
     //=include modules.js
 });
